d8749f74c1a138e5879fb0bf7b6181244d66eb02
Raul Cisneros <raul.f.cisneros@outlook.com>
2020-05-19 12:04:19 -0700

    adding comments

--- Program.cs
+++ Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSC205_ch2
+namespace MSSA_NAMESPACE
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //GETTING STARTED WITH COMMETS
+            /*
+            Console.WriteLine("Welcom to MSSA 1"); //ctrl+F5
+            Console.WriteLine("Welcom to MSSA 2"); //ctrl+F5
+            Console.WriteLine("Welcom to MSSA 3"); //ctrl+F5
+            */
+
 
         }
     }
