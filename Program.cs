ded3f58871543dff34f9b1f074c9621d67ac4dec
Raul Cisneros <raul.f.cisneros@outlook.com>
2020-05-19 14:03:14 -0700

    End of class

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +17,54 @@ namespace MSSA_NAMESPACE
             Console.WriteLine("Welcom to MSSA 2"); //ctrl+F5
             Console.WriteLine("Welcom to MSSA 3"); //ctrl+F5
             */
+
+            //string myName = "Raul";
+            //myName = Convert.ToString(2020);
+
+
+            //int x = 2021;
+            //string bob = "MSSA";
+            //string myStudent = "y";
+            //Console.WriteLine("bob"); // displasy bob
+            //Console.WriteLine(bob); //displays MSSA
+            //Console.WriteLine("bob" + bob + " " + 2008+ " " + x + myStudent);
+
+            //int age; //declaring a variable
+            //double gpa = 4.0; //declaring ab initializing a variable
+
+
+            //double num1 = 7, num2 = 2;
+            //Console.WriteLine((7/2); // in python 7/2 = 3.5, in C# 7/2 = 3
+            //Console.WriteLine(7.0/2);//this is good
+            //Console.WriteLine(num1/num2); //integer division !!!
+            //Console.WriteLine(Convert.ToDouble(num1) / num2); //this is good
+            //Console.WriteLine( ((double) num1) / num2); //this is good
+
+            //int num3 = 7;
+            //double num4 = 2;
+            //Console.WriteLine(num3/ num4); //this is good
+
+            //int sec = 0;
+            //while(true)
+            //{
+            //    Console.WriteLine(sec)
+            //        Thread.Sleep;
+            //    sec = sec(sec + 1) % 7;
+
+
+            Console.Write("Enter yourfirst name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine(firstName.ToUpper());
+
+            Console.WriteLine("Enter your age: ");
+            int age2; = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($)
+
+
+
+
+
 
 
         }
